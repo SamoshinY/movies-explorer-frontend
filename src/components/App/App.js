366decac62a62c0f132ec8470e2099ca48e3cc1e
SamoshinY <113671121+SamoshinY@users.noreply.github.com>
2023-05-31 23:55:55 +0700

    создать файловую структуру

---
+++ src/components/App/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from '../Header/Header';
+import Navigation from '../Navigation/Navigation';
+import Main from '../Main/Main';
+import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
+import Login from '../Login/Login';
+import Register from '../Register/Register';
+import Footer from '../Footer/Footer';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/saved-movies" element={<SavedMovies />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<Login />} />
+        <Route path="/signup" element={<Register />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
